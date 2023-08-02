dbc540cdaa194352e1c3517eec90d4e7aa2b306f
hunggahot <hunggahot@gmail.com>
2023-08-02 12:03:40 +0700

    Get All Employees API

--- src/main/java/com/example/miniprojectbackend/MiniProjectBackendApplication.java
+++ src/main/java/com/example/miniprojectbackend/MiniProjectBackendApplication.java
@@ -1,13 +1,34 @@
 package com.example.miniprojectbackend;
 
+import com.example.miniprojectbackend.model.Employee;
+import com.example.miniprojectbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiniProjectBackendApplication {
+public class MiniProjectBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniProjectBackendApplication.class, args);
 	}
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = new Employee();
+		employee.setFirstName("Lam");
+		employee.setLastName("Hung");
+		employee.setEmailId("hung@gmail.com");
+		employeeRepository.save(employee);
+
+		Employee employee1 = new Employee();
+		employee1.setFirstName("Tran");
+		employee1.setLastName("Tuan");
+		employee1.setEmailId("tuan@gmail.com");
+		employeeRepository.save(employee1);
+	}
 }
