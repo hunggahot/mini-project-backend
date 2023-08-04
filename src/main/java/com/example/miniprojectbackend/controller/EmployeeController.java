e442200242a4024fc47749fa8522be9dc33fd74d
hunggahot <hunggahot@gmail.com>
2023-08-04 10:05:05 +0700

    Create Employee

--- src/main/java/com/example/miniprojectbackend/controller/EmployeeController.java
+++ src/main/java/com/example/miniprojectbackend/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.example.miniprojectbackend.controller;
 import com.example.miniprojectbackend.model.Employee;
 import com.example.miniprojectbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +17,11 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    // build create employee REST API
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
