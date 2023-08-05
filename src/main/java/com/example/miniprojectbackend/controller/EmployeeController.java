f54f58b618bd5f9ecffb32498ead3364de2b5911
hunggahot <hunggahot@gmail.com>
2023-08-05 14:47:31 +0700

    Delete Employee

--- src/main/java/com/example/miniprojectbackend/controller/EmployeeController.java
+++ src/main/java/com/example/miniprojectbackend/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.example.miniprojectbackend.exception.ResourceNotFoundException;
 import com.example.miniprojectbackend.model.Employee;
 import com.example.miniprojectbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -48,5 +49,15 @@ public class EmployeeController {
         employeeRepository.save(updateEmployee);
 
         return ResponseEntity.ok(updateEmployee);
+    }
+
+    // build delete employee REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+        employeeRepository.delete(employee);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
