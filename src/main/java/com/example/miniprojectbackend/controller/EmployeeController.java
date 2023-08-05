b74cf79546b4de2f0f95559ee742099d10c1ebb3
hunggahot <hunggahot@gmail.com>
2023-08-05 13:10:15 +0700

    Get Employee By Id

--- src/main/java/com/example/miniprojectbackend/controller/EmployeeController.java
+++ src/main/java/com/example/miniprojectbackend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.miniprojectbackend.controller;
 
+import com.example.miniprojectbackend.exception.ResourceNotFoundException;
 import com.example.miniprojectbackend.model.Employee;
 import com.example.miniprojectbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,4 +26,13 @@ public class EmployeeController {
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
+
+    // build get employee by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+        return ResponseEntity.ok(employee);
+    }
+
 }
